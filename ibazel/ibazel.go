82f14f130cd68c0e8d95323337f81adc3c669d56
Jared Neil <jaredneil@lucidchart.com>
2022-10-12 16:59:58 -0600

    Terminate before rebuild for run, not just mrun

--- ibazel/ibazel.go
+++ ibazel/ibazel.go
@@ -366,6 +366,7 @@ func (i *IBazel) iteration(command string, commandToRun runnableCommand, targets
 			i.state = RUN
 		}
 	case RUN:
+		i.cmd.BeforeRebuild()
 		log.Logf("%s %s", strings.Title(verb(command)), joinedTargets)
 		i.beforeCommand(targets, command)
 		outputBuffer, err := commandToRun(targets...)
